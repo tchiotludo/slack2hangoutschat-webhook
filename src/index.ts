fd9686060a20d7c9538527ab2a99ae3e7b98e17d
tchiotludo <tchiot.ludo@gmail.com>
2019-12-10 09:57:50 +0100

    Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export {Webhook} from "./webhook";
-export {Converter} from "./converter";
-export {app} from "./server";
+export { Webhook } from "./webhook";
+export { Converter } from "./converter";
+export { app } from "./server";
